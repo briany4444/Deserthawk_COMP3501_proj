d6ccb820b592d166ad9234fd8b28991b3d5a8a4c
Liam Stickle <lagstickle@gmail.com>
2023-12-03 22:32:33 -0500

    leaving the paths commented in for ease

--- broken_camera/path_config.h
+++ broken_camera/path_config.h
@@ -1,3 +1,4 @@
-#define MATERIAL_DIRECTORY "C:/Users/User/3501_Proj/broken_camera"
+#define MATERIAL_DIRECTORY "C:\\Users\\lagst\\OneDrive\\Documents\\GitHub\\3501_Proj\\broken_camera"
+//C:\\Users\\lagst\\OneDrive\\Documents\\GitHub\\3501_Proj\\broken_camera
 
 // change to specify your own location here
